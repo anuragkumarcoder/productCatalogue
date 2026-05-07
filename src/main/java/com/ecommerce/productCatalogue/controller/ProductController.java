1fb3e91734a85390cb75fdb8953eedad4095c199
anurag <anuragkumar78702685001@gmail.com>
2026-05-07 21:49:03 +0530

    done the basic mapping and amde controllers

---
+++ src/main/java/com/ecommerce/productCatalogue/controller/ProductController.java
@@ -0,0 +1,26 @@
+package com.ecommerce.productCatalogue.controller;
+
+import com.ecommerce.productCatalogue.model.Product;
+import com.ecommerce.productCatalogue.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+    @GetMapping
+    public List<Product> getAllProducts(){
+        return productService.getAllProducts();
+    }
+    @GetMapping("/category/{categoryId}")
+    public List<Product> getAllProductByCategory(@PathVariable  Long CategoryId){
+        return productService.getProductsByCategory(CategoryId);
+    }
+}
---
+++ src/main/java/com/ecommerce/productCatalogue/service/ProductService.java
@@ -0,0 +1,21 @@
+package com.ecommerce.productCatalogue.service;
+
+import com.ecommerce.productCatalogue.model.Product;
+import com.ecommerce.productCatalogue.repo.ProductRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepo productRepo;
+
+    public List<Product> getAllProducts() {
+        return productRepo.findAll();
+    }
+    public List<Product> getProductsByCategory(Long CategoryId){
+        return productRepo.findByCategoryId(CategoryId);
+    }
+}
